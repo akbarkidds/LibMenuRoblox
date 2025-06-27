c3c5bbc50a8e1b267aa46b8e048c9b9e01164623
akbarkidds <55218323+akbarkidds@users.noreply.github.com>
2025-06-27 23:28:33 +0800

    Update infiniteYield.lua

--- iy/infiniteYield.lua
+++ iy/infiniteYield.lua
@@ -6426,7 +6426,7 @@ local TeleportCheck = false
 Players.LocalPlayer.OnTeleport:Connect(function(State)
 	if KeepInfYield and (not TeleportCheck) and queueteleport then
 		TeleportCheck = true
-		queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()")
+		queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/akbarkidds/LibMenuRoblox/refs/heads/main/iy/infiniteYield.lua'))()")
 	end
 end)
 
